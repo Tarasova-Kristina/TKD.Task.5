851d1b4b8d667033a33bdc3e0ece2a8ad006ff7f
Kezha88 <kuku2304@gmail.com>
2023-03-20 19:57:07 +0300

    Добавьте файлы проекта.

---
+++ TKD.Task.5/View/Pages/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using TKD.Task._5.Core;
+using TKD.Task._5.View.Pages.TaskPages;
+using System.Windows;
+using System.Windows.Controls;
+namespace TKD.Task._5.View.Pages
+{
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
